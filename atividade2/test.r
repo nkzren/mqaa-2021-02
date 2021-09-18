55d05b96f07e409b4bafaccceaf4a7271af44945
nkzren <renan.nakazawa0@gmail.com>
2021-09-18 15:14:20 -0300

    adiciona plot de distribuicao normal

--- atividade2/test.r
+++ atividade2/test.r
@@ -1,11 +1,23 @@
+# Carrega o dataset
 df <- read.table("atividade2/dataset_PT.csv", header = TRUE, sep = ';', stringsAsFactors = FALSE)
 
-split <- df[1:100, 1:10]
+split <- df[, 1:10]
+equips <- list(split$Equipamentos_para_Aulas_Online)
+
+equips_col <- split[, 7]
+
+global_mean <- mean(equips_col)
+global_sd <- sd(equips_col)
+
+# equip_mean <- aggregate(equips_col, equips, mean)
+# equip_sd <- aggregate(equips_col, equips, sd)
+
+y <- dnorm(equips_col, global_mean, global_sd)
+
+# plot do grafico para verificar se a coluna desejada tem distribuicao normal
+plot(equips_col, y)
+
 anova <- aov(Tempo_em_estudo_individual ~ Equipamentos_para_Aulas_Online, data = df)
 
-summary(anova)
-# print(aggregate(split[, 4], list(split$Equipamentos_para_Aulas_Online), mean))
-# print(list(df$Regiao_de_Residencia))
-# print(df[1:10, 2:6]$Regiao_de_Residencia)
-
+print(summary(anova))
 
