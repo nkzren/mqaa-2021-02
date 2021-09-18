fab3c96542f1a15f682baeb3f91c3314dd801ca8
Luan Monteiro <luan.monteiro@spacesheepgames.com>
2021-09-18 15:07:46 -0300

    Added anova analysis

--- atividade2/test.r
+++ atividade2/test.r
@@ -1,6 +1,11 @@
 df <- read.table("atividade2/dataset_PT.csv", header = TRUE, sep = ';', stringsAsFactors = FALSE)
 
 split <- df[1:100, 1:10]
-print(aggregate(split[, 4], list(split$Equipamentos_para_Aulas_Online), mean))
+anova <- aov(Tempo_em_estudo_individual ~ Equipamentos_para_Aulas_Online, data = df)
+
+summary(anova)
+# print(aggregate(split[, 4], list(split$Equipamentos_para_Aulas_Online), mean))
 # print(list(df$Regiao_de_Residencia))
 # print(df[1:10, 2:6]$Regiao_de_Residencia)
+
+
